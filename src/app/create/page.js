f61dd2fa199d6c107697ff48cfa2a2397172e172
alikerock <alikerock@gmail.com>
2026-01-15 15:33:10 +0900

    feat: 새글 추가 및 저장

--- src/app/create/page.js
+++ src/app/create/page.js
@@ -1,9 +1,39 @@
+"use client";
+import { useRouter } from 'next/navigation'
+
 export default function Create() {
+  const router = useRouter();
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    const title = e.target.title.value;
+    const message = e.target.message.value;
+    const options = {
+      method:'POST',
+      headers:{
+        'Content-Type':'application/json'
+      },
+      body:JSON.stringify({title,message}) //입력한 값을 json 문자열 형태로 변환
+    }
+    fetch('http://localhost:9999/topics', options)
+    .then(res=>res.json())
+    .then(result=>{      
+      router.push(`/read/${result.id}`);
+    });
+  }
+
   return (
     <>
-      <form>
-        <input type="text"/>
-      </form> 
+     <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <input type="text" className="form-control" name="title" placeholder="title"/>
+        </div>
+        <div className="mb-3">          
+          <textarea className="form-control" name="message" rows="3"></textarea>
+        </div>
+        <p>
+          <button type="submit" className="btn btn-success">전송</button>
+        </p>
+      </form>   
     </>
   );
 }
