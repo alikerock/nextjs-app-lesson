e2ca2701b4a175f3596b1d1a8870050c79427017
alikerock <alikerock@gmail.com>
2026-01-16 10:31:22 +0900

    feat: 글 삭제

--- src/app/Control.js
+++ src/app/Control.js
@@ -1,13 +1,25 @@
 "use client";
 //클라이언 컴포넌트용 useParams 함수, 주소표시줄의 id 파악
 import Link from "next/link";
-import { useParams } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 
 export default function Control() {
   const params = useParams();
   const id = params?.id;
+  const router = useRouter();
 
-  console.log(params);
+  const handleDelete = ()=>{
+    const options = {
+      method:'DELETE'     
+    }
+    fetch(`http://localhost:9999/topics/${id}`, options)
+    .then(res=>res.json())
+    .then(()=>{
+      router.push('/');//삭제후 홈으로 이동
+      router.refresh();//이동한 페이지 새로고침
+    });
+  }
+
   return (
     <ul className="nav gap-1">
       <li>
@@ -18,7 +30,7 @@ export default function Control() {
           <li>
             <Link href={`/update/${id}`} className="btn btn-secondary">Update</Link>
           </li>
-          <li><button type="button" className="btn btn-danger">Delete</button></li>
+          <li><button type="button" className="btn btn-danger" onClick={handleDelete}>Delete</button></li>
         </>
       }
     </ul>
