ba6f2a0b3b32464644cc015357983eae643ed6c5
alikerock <alikerock@gmail.com>
2026-01-16 10:13:44 +0900

    feat: id구분 버튼 표시 변경

--- src/app/Control.js
+++ src/app/Control.js
@@ -1,15 +1,26 @@
+"use client";
+//클라이언 컴포넌트용 useParams 함수, 주소표시줄의 id 파악
 import Link from "next/link";
+import { useParams } from "next/navigation";
 
 export default function Control() {
+  const params = useParams();
+  const id = params?.id;
+
+  console.log(params);
   return (
     <ul className="nav gap-1">
       <li>
         <Link href="/create" className="btn btn-primary">Create</Link>
       </li>
-      <li>
-        <Link href="/update/1" className="btn btn-secondary">Update</Link>
-      </li>
-      <li><button type="button" className="btn btn-danger">Delete</button></li>
+      {
+        id && <>
+          <li>
+            <Link href={`/update/${id}`} className="btn btn-secondary">Update</Link>
+          </li>
+          <li><button type="button" className="btn btn-danger">Delete</button></li>
+        </>
+      }
     </ul>
   )
 }
